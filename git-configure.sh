c3197e8f228fd0cfc091cafa9a7c3ebfc5cc49c3
paulbknhs <dev@paulbknhs.de>
2024-12-16 22:55:49 +0100

    init

---
+++ git-configure.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+git config --global user.name "paulbknhs"
+git config --global user.email "dev@paulbknhs.de"
+git config --global init.defaultBranch "main"
+git config --global core.editor "nvim"
+
+git remote set-url origin --push git@github.com:paulbknhs/arch2go.git
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+CONFIG="$HOME/.config"
+ZSH="$CONFIG/zsh"
+NVIM="$CONFIG/nvim"
+HYPR="$CONFIG/hypr"
+WAYBAR="$CONFIG/waybar"
+KITTY="$CONFIG/kitty"
+
+sudo pacman --noconfirm -Syu
+sudo pacman -noconfirm -S base base-devel git curl wget zsh lsd fzf bat
+
+# SSH
+ssh-keygen -t ed25519
+/bin/cat "$HOME/.ssh/id_ed25519.pub"
+read -n 1 -p "Copy and add this ssh-key to Github! Press RETURN to continue."
+
+mkdir -p "$HOME/.config"
+[[ -d $NVIM ]] || git clone https://github.com/paulbknhs/nvim.git "$NVIM"
+[[ -d $HYPR ]] || git clone https://github.com/paulbknhs/hypr.git "$HYPR"
+[[ -d $WAYBAR ]] || git clone https://github.com/paulbknhs/waybar.git "$WAYBAR"
+[[ -d $KITTY ]] || git clone https://github.com/paulbknhs/kitty.git "$KITTY"
+[[ -d $ZSH ]] || git clone https://github.com/paulbknhs/zsh.git "$ZSH"
+
+sudo ln -sf "$ZSH"/rc.zsh "$HOME"/.zshrc
+sudo ln -sf "$ZSH"/zsh_plugins.txt "$HOME"/.zsh_plugins.txt
+
+cd "$NVIM" && git remote set-url origin --push git@github.com:paulbknhs/nvim.git
+cd "$HYPR" && git remote set-url origin --push git@github.com:paulbknhs/hypr.git
+cd "$WAYBAR" && git remote set-url origin --push git@github.com:paulbknhs/waybar.git
+cd "$KITTY" && git remote set-url origin --push git@github.com:paulbknhs/kitty.git
+cd "$ZSH" && git remote set-url origin --push git@github.com:paulbknhs/zsh.git
+
+chsh "$USER"
