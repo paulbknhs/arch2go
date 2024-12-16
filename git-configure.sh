77f1eb8a0e970aae95cb71f98a7f4774fecdff02
paulbknhs <dev@paulbknhs.de>
2024-12-16 22:56:39 +0100

    added push.autoSetupRemote

--- git-configure.sh
+++ git-configure.sh
@@ -4,5 +4,5 @@ git config --global user.name "paulbknhs"
 git config --global user.email "dev@paulbknhs.de"
 git config --global init.defaultBranch "main"
 git config --global core.editor "nvim"
-
+git config --global push.autoSetupRemote true
 git remote set-url origin --push git@github.com:paulbknhs/arch2go.git
